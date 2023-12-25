e5125f3235017c147aa4819f73392382302ed609
Sarthak <106574498+slice-sarthakd@users.noreply.github.com>
2023-12-25 21:29:23 +0530

    feat: entry point for cluster and husky configured

---
+++ husky/cloud/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cargo build --release --bin entangledb
+
+for ID in 1 2 3 4 5; do
+    (cargo run -q --release -- -c entangledb$ID/entangledb.yaml 2>&1 | sed -e "s/\\(.*\\)/entangledb$ID \\1/g") &
+done
+
+trap 'kill $(jobs -p)' EXIT
+wait < <(jobs -p)
