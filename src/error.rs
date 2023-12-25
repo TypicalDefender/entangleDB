2141b22c11d23bc4f44afed3d0d9c528bd6574ca
Sarthak <106574498+slice-sarthakd@users.noreply.github.com>
2023-12-25 23:52:16 +0530

    [fix]: storage library and error

---
+++ src/error.rs
@@ -0,0 +1,164 @@
+use serde_derive::{Deserialize, Serialize};
+use std::fmt::{self, Display};
+
+/// Result returning Error
+pub type Result<T> = std::result::Result<T, Error>;
+
+/// entangledb errors. All except Internal are considered user-facing.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub enum Error {
+    Abort,
+    Config(String),
+    Internal(String),
+    Parse(String),
+    ReadOnly,
+    Serialization,
+    Value(String),
+}
+
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Config(s) | Error::Internal(s) | Error::Parse(s) | Error::Value(s) => {
+                write!(f, "{}", s)
+            }
+            Error::Abort => write!(f, "Operation aborted"),
+            Error::Serialization => write!(f, "Serialization failure, retry transaction"),
+            Error::ReadOnly => write!(f, "Read-only transaction"),
+        }
+    }
+}
+
+impl serde::ser::Error for Error {
+    fn custom<T: Display>(msg: T) -> Self {
+        Error::Internal(msg.to_string())
+    }
+}
+
+impl serde::de::Error for Error {
+    fn custom<T: Display>(msg: T) -> Self {
+        Error::Internal(msg.to_string())
+    }
+}
+
+impl From<Box<bincode::ErrorKind>> for Error {
+    fn from(err: Box<bincode::ErrorKind>) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<config::ConfigError> for Error {
+    fn from(err: config::ConfigError) -> Self {
+        Error::Config(err.to_string())
+    }
+}
+
+impl From<hex::FromHexError> for Error {
+    fn from(err: hex::FromHexError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<log::ParseLevelError> for Error {
+    fn from(err: log::ParseLevelError) -> Self {
+        Error::Config(err.to_string())
+    }
+}
+
+impl From<log::SetLoggerError> for Error {
+    fn from(err: log::SetLoggerError) -> Self {
+        Error::Config(err.to_string())
+    }
+}
+
+impl From<regex::Error> for Error {
+    fn from(err: regex::Error) -> Self {
+        Error::Value(err.to_string())
+    }
+}
+
+impl From<rustyline::error::ReadlineError> for Error {
+    fn from(err: rustyline::error::ReadlineError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<std::array::TryFromSliceError> for Error {
+    fn from(err: std::array::TryFromSliceError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<std::num::TryFromIntError> for Error {
+    fn from(err: std::num::TryFromIntError) -> Self {
+        Error::Value(err.to_string())
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<std::net::AddrParseError> for Error {
+    fn from(err: std::net::AddrParseError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<std::num::ParseFloatError> for Error {
+    fn from(err: std::num::ParseFloatError) -> Self {
+        Error::Parse(err.to_string())
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(err: std::num::ParseIntError) -> Self {
+        Error::Parse(err.to_string())
+    }
+}
+
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(err: std::string::FromUtf8Error) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl<T> From<std::sync::PoisonError<T>> for Error {
+    fn from(err: std::sync::PoisonError<T>) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<tokio::task::JoinError> for Error {
+    fn from(err: tokio::task::JoinError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<tokio::sync::mpsc::error::TryRecvError> for Error {
+    fn from(err: tokio::sync::mpsc::error::TryRecvError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl<T> From<tokio::sync::mpsc::error::SendError<T>> for Error {
+    fn from(err: tokio::sync::mpsc::error::SendError<T>) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl<T> From<tokio::sync::mpsc::error::TrySendError<T>> for Error {
+    fn from(err: tokio::sync::mpsc::error::TrySendError<T>) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
+
+impl From<tokio::sync::oneshot::error::RecvError> for Error {
+    fn from(err: tokio::sync::oneshot::error::RecvError) -> Self {
+        Error::Internal(err.to_string())
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![warn(clippy::all)]
+#![allow(clippy::new_without_default)]
+#![allow(clippy::unneeded_field_pattern)]
+
+pub mod client;
+pub mod error;
+pub mod raft;
+pub mod server;
+pub mod sql;
+pub mod storage;
+
+pub use client::Client;
+pub use server::Server;
